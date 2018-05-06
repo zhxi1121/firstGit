3a95171abc7be1a6d915b6d357907849adba38e6
zhengxiao <zhengxiao@zhengxiao>
2018-05-06 17:35:20 +0800

    properties读取demo

--- firstDemo/src/com/test/demo/FirstDemo.java
+++ firstDemo/src/com/test/demo/FirstDemo.java
@@ -2,7 +2,6 @@ package com.test.demo;
 
 public class FirstDemo {
     public static void main(String[] args) {
-        System.out.println("1333666663");
-        System.out.println();
+        
     }
 }
